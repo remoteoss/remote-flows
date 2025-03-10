47066ddfef93ef642cd6424f860ed6db8aa984a4
Gabriel Garcia <gabriel.garcia@remote.com>
2025-03-10 15:39:44 +0100

    export all types from API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import './styles/global.css';
 
-// @TODO: Only for testing purposes. It should be removed when CostCalculator is ready
-export { SelectField } from '@/src/components/form/fields/SelectField';
 export { RemoteFlows } from '@/src/RemoteFlowsProvider';
 export { ThemeProvider } from '@/src/theme';
+export * from './client/types.gen';
