ddf172a5c9025b253139cbc7f0e252fbd22e1842
Gabriel Garcia <gabriel.garcia@remote.com>
2025-03-10 10:19:58 +0100

    fix types

--- src/components/form/fields/fieldsMapping.tsx
+++ src/components/form/fields/fieldsMapping.tsx
@@ -5,7 +5,7 @@ import { RadioGroupField } from '@/src/components/form/fields/RadioGroupField';
 import { SelectField } from '@/src/components/form/fields/SelectField';
 import { $TSFixMe } from '@remoteoss/json-schema-form';
 
-export const fieldsMapConfig = {
+export const fieldsMapConfig: Record<string, React.ComponentType<any>> = {
   radio: RadioGroupField,
   number: (props: $TSFixMe) => <TextField type="number" {...props} />,
   fieldset: FieldSetField,
