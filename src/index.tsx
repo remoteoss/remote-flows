ee2f4ec2697ae654835031de80a0f9fc5f5d2647
Joao Almeida <joao.mnp.almeida@gmail.com>
2025-03-13 09:53:25 +0000

    fix a couple of eslint warnings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import './styles/global.css';
 
-export { RemoteFlows } from '@/src/RemoteFlowsProvider';
 export { CostCalculator } from '@/src/flows/CostCalculator';
+export { RemoteFlows } from '@/src/RemoteFlowsProvider';
 export { ThemeProvider } from '@/src/theme';
+// eslint-disable-next-line react-refresh/only-export-components
 export * from './client/types.gen';
