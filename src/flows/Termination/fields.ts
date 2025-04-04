6806f5bde007e88184d7d7bac16a83dea1d7f335
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-04 09:14:25 +0200

    fields

--- src/flows/Termination/fields.ts
+++ src/flows/Termination/fields.ts
@@ -23,4 +23,34 @@ export const fields: Field[] = [
     schema: string().required('Request is required'),
     type: 'string',
   },
+  {
+    description: '',
+    inputType: 'radio',
+    isVisible: true,
+    label: 'Have you informed the employee of the termination?',
+    name: 'customer_informed_employee',
+    options: [
+      {
+        label: 'Yes',
+        value: 'yes',
+      },
+      {
+        label: 'No',
+        value: 'no',
+      },
+    ],
+    required: true,
+    schema: string().required('Customer is required'),
+    type: 'string',
+  },
+  {
+    description: 'We’ll use this for post-termination communication.',
+    inputType: 'text',
+    isVisible: true,
+    label: 'Employee’s personal email',
+    name: 'employer_confirmed_email',
+    required: true,
+    schema: string().required('Personal email is required'),
+    type: 'string',
+  },
 ];
