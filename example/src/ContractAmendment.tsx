f63bd64ab518251c67ad367cb096685c255a8bca
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-08 13:09:24 +0200

    contract amendment

--- example/src/ContractAmendment.tsx
+++ example/src/ContractAmendment.tsx
@@ -1,18 +1,36 @@
-import { ContractAmendmentFlow } from '@remoteoss/remote-flows';
+import { ContractAmendmentFlow, RemoteFlows, ContractAmendmentForm, ContractAmendmentSubmit } from '@remoteoss/remote-flows';
 
 export const ContractAmendmentDemo = () => {
+  const fetchToken = () => {
+    return fetch('/api/token')
+      .then((res) => res.json())
+      .then((data) => ({
+        accessToken: data.access_token,
+        expiresIn: data.expires_in,
+      }))
+      .catch((error) => {
+        console.error({ error });
+        throw error;
+      });
+  };
   return (
-    <ContractAmendmentFlow
-      countryCode="PRT"
-      employmentId="87b7f5c9-6b9a-4bcb-b23d-b359a47b6a33"
-      render={({ contractAmendmentBag }) => {
-        return (
-          <div>
-            <h1>Contract Amendment</h1>
-            <pre>{JSON.stringify(contractAmendmentBag, null, 2)}</pre>
-          </div>
-        );
-      }}
-    />
+    <RemoteFlows auth={() => fetchToken()}>
+      <ContractAmendmentFlow
+        countryCode="PRT"
+        employmentId="87b7f5c9-6b9a-4bcb-b23d-b359a47b6a33"
+        render={({ contractAmendmentBag }) => {
+          if(contractAmendmentBag.isLoading) {
+            return <p>Loading...</p>;
+          }
+          
+          return (
+            <>
+            <ContractAmendmentForm />
+            <ContractAmendmentSubmit />
+            </>
+          );
+        }}
+      />
+    </RemoteFlows>
   );
 };
