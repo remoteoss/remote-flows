85a5326a4f837cfc966401f008a54317672e3228
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-08 11:09:29 +0200

    remove only

--- src/flows/CostCalculator/tests/hooks.test.tsx
+++ src/flows/CostCalculator/tests/hooks.test.tsx
@@ -33,7 +33,7 @@ describe('useCostCalculator', () => {
     queryClient.clear();
   });
 
-  test.only('should load regions when a country with regions is selected', async () => {
+  test('should load regions when a country with regions is selected', async () => {
     const { result } = renderHook(() => useCostCalculator(), { wrapper });
 
     await waitFor(() => {
