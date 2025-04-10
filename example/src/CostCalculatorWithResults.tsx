12c3503b9c8bdfc39f88ca554a17b3d1db1cb3c5
Joao Almeida <joao.mnp.almeida@gmail.com>
2025-04-10 09:29:04 +0100

    Revert "Reapply "update README""
    This reverts commit b775abea10015fa0124eb864c33031ffef6ed088.

--- example/src/CostCalculatorWithResults.tsx
+++ example/src/CostCalculatorWithResults.tsx
@@ -1,16 +1,16 @@
-import type { CostCalculatorEstimateResponse } from '@remoteoss/remote-flows';
 import {
   CostCalculatorFlow,
   CostCalculatorForm,
-  CostCalculatorResetButton,
-  CostCalculatorResults,
   CostCalculatorSubmitButton,
+  CostCalculatorResetButton,
   RemoteFlows,
+  CostCalculatorResults,
 } from '@remoteoss/remote-flows';
-import { useState } from 'react';
+import type { CostCalculatorEstimateResponse } from '@remoteoss/remote-flows';
 import Flag from 'react-flagpack';
-import 'react-flagpack/dist/style.css';
 import './App.css';
+import 'react-flagpack/dist/style.css';
+import { useState } from 'react';
 
 const estimationOptions = {
   title: 'Estimate for a new company',
@@ -35,49 +35,7 @@ export function CostCalculatorWithResults() {
   };
 
   return (
-    <RemoteFlows
-      components={{
-        number: ({ field, fieldState, fieldData }) => {
-          console.log(fieldData);
-          return (
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <label>{fieldData.label}</label>
-              <input {...field} style={{ border: '1px solid pink' }} />
-              {fieldState.error && (
-                <span className="text-red-500">{fieldState.error.message}</span>
-              )}
-            </div>
-          );
-        },
-        select: ({ field, fieldState, fieldData }) => {
-          console.log('select', fieldData);
-          return (
-            <>
-              <select
-                {...field}
-                onChange={(ev) => {
-                  field.onChange(ev.target.value);
-                }}
-                style={{ border: '1px solid pink' }}
-              >
-                {fieldData?.options?.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-              {fieldState.error && (
-                <span className="text-red-500">{fieldState.error.message}</span>
-              )}
-            </>
-          );
-        },
-        text: ({ field, metadata }) => {
-          return <input {...field} style={{ border: '1px solid black' }} />;
-        },
-      }}
-      auth={() => fetchToken()}
-    >
+    <RemoteFlows auth={() => fetchToken()}>
       <CostCalculatorFlow
         estimationOptions={estimationOptions}
         render={(props) => {
