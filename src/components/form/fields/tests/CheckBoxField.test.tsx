960d4e060bbee6f471d94bc539e221f628465f12
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-23 15:49:49 +0200

    add more tests

--- src/components/form/fields/tests/CheckBoxField.test.tsx
+++ src/components/form/fields/tests/CheckBoxField.test.tsx
@@ -168,4 +168,26 @@ describe('CheckBoxField Component', () => {
 
     expect(mockOnChange).toHaveBeenCalledWith(true, 'option1');
   });
+
+  it("should call handleCheckboxChange with 'false' when checkbox is clicked 2 times", () => {
+    const options = [
+      { label: 'Option 1', value: 'option1' },
+      { label: 'Option 2', value: 'option2' },
+    ];
+
+    const props = {
+      ...defaultProps,
+      multiple: true,
+      options,
+      onChange: mockOnChange,
+    };
+
+    renderWithFormContext(props);
+
+    const checkbox = screen.getByLabelText('Option 1');
+    fireEvent.click(checkbox);
+    fireEvent.click(checkbox);
+
+    expect(mockOnChange).toHaveBeenCalledWith(false, 'option1');
+  });
 });
