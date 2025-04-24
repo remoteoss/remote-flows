9e91bfb34508efc1fe36520ac867c67f7d7d185e
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-24 11:31:46 +0200

    add docs

--- src/flows/Termination/TerminationFlow.tsx
+++ src/flows/Termination/TerminationFlow.tsx
@@ -2,16 +2,33 @@ import { TerminationContext } from '@/src/flows/Termination/context';
 import React, { PropsWithChildren, useId } from 'react';
 import { useTermination } from '@/src/flows/Termination/hooks';
 import { JSFModify } from '@/src/flows/CostCalculator/types';
+import { TerminationForm } from '@/src/flows/Termination/TerminationForm';
+import { TerminationSubmit } from '@/src/flows/Termination/TerminationSubmit';
+
+type RenderProps = {
+  /**
+   * The termination amendment bag returned by the useTermination hook.
+   * This bag contains all the methods and properties needed to handle the termination flow.
+   * @see {@link useTermination}
+   */
+  terminationBag: ReturnType<typeof useTermination>;
+  /**
+   * The components used in the termination flow.
+   * This includes the form, submit button.
+   * @see {@link TerminationForm}
+   * @see {@link TerminationSubmit}
+   */
+  components: {
+    Form: typeof TerminationForm;
+    SubmitButton: typeof TerminationSubmit;
+  };
+};
 
 function TerminationFlowProvider({
   render,
   terminationBag,
 }: PropsWithChildren<{
-  render: ({
-    terminationBag,
-  }: {
-    terminationBag: ReturnType<typeof useTermination>;
-  }) => React.ReactNode;
+  render: ({ terminationBag }: RenderProps) => React.ReactNode;
   terminationBag: ReturnType<typeof useTermination>;
 }>) {
   const formId = useId();
@@ -23,7 +40,13 @@ function TerminationFlowProvider({
         terminationBag,
       }}
     >
-      {render({ terminationBag })}
+      {render({
+        terminationBag,
+        components: {
+          Form: TerminationForm,
+          SubmitButton: TerminationSubmit,
+        },
+      })}
     </TerminationContext.Provider>
   );
 }
