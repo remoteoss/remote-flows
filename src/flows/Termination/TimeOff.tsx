c44d285dc13e1f07a384e9665d552f7808cb235c
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-25 15:33:03 +0200

    simplify timeoff

--- src/flows/Termination/TimeOff.tsx
+++ src/flows/Termination/TimeOff.tsx
@@ -1,9 +1,13 @@
-import { Button } from '@/src/components/ui/button';
 import { useEmploymentQuery, useTimeOffQuery } from '@/src/data/hooks';
 import { useTerminationContext } from '@/src/flows/Termination/context';
 import React from 'react';
 
-export const TimeOff = ({ onOpenModal }: { onOpenModal: () => void }) => {
+export const TimeOff = ({
+  children,
+}: {
+  onOpenModal: () => void;
+  children?: React.ReactNode;
+}) => {
   const { terminationBag } = useTerminationContext();
   const employment = useEmploymentQuery({
     employmentId: terminationBag.employmentId,
@@ -21,21 +25,12 @@ export const TimeOff = ({ onOpenModal }: { onOpenModal: () => void }) => {
   // if days is 0 or > 1 'days' else 'day
   const daysLiteral = days > 1 || days === 0 ? 'days' : 'day';
 
-  const handleModal = (e: React.MouseEvent<HTMLButtonElement>): void => {
-    e.preventDefault();
-    onOpenModal();
-  };
-
   return (
     <div className="RemoteFlows__TimeOff">
       <p className="RemoteFlows__TimeOff__Title">
         We have recorded {days} {daysLiteral} of paid time off for {username}
       </p>
-      <Button asChild variant="link" onClick={handleModal}>
-        <a href="#" className="RemoteFlows__TimeOff__link">
-          See {username}'s time off breakdown
-        </a>
-      </Button>
+      {children}
     </div>
   );
 };
