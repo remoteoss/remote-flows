21ba597ee53dce616b96b2f519113b63cb37a6d1
Gabriel Garcia <gabriel.garcia@remote.com>
2025-04-30 11:28:11 +0200

    remove logs

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -145,13 +145,6 @@ if (DOMPurify.isSupported) {
   DOMPurify.addHook('afterSanitizeAttributes', (node) => {
     const target = node.getAttribute('target');
 
-    console.log({
-      target,
-      nod: node.tagName === 'A',
-      anchor: node.getAttribute('href'),
-      anchorTarget: node.getAttribute('target'), // Changed from getAttributes() to getAttribute('href')
-    });
-
     // set value of target to be _blank with rel, or keep as _self if already set
     if (node.tagName === 'A' && (!target || target !== '_self')) {
       node.setAttribute('target', '_blank');
