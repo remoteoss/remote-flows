d53b634a54fa3fc06fc4f2392af5b3596806ee0a
Gabriel García Seco <ggarciaseco@gmail.com>
2025-05-07 14:39:06 +0200

    Revert "Add MoneyField component and corresponding tests (#123)"
    This reverts commit 7ceb21f6c3b335ff66109cd6e894501844fa6982.

--- src/components/form/fields/fieldsMapping.tsx
+++ src/components/form/fields/fieldsMapping.tsx
@@ -1,14 +1,13 @@
 import { CheckBoxField } from '@/src/components/form/fields/CheckBoxField';
 import { DatePickerField } from '@/src/components/form/fields/DatePickerField';
-import { EmailField } from '@/src/components/form/fields/EmailField';
 import { FieldSetField } from '@/src/components/form/fields/FieldSetField';
 import { FileUploadField } from '@/src/components/form/fields/FileUploadField';
-import { MoneyField } from '@/src/components/form/fields/MoneyField';
 import { NumberField } from '@/src/components/form/fields/NumberField';
 import { RadioGroupField } from '@/src/components/form/fields/RadioGroupField';
 import { SelectField } from '@/src/components/form/fields/SelectField';
 import { TextAreaField } from '@/src/components/form/fields/TextAreaField';
 import { TextField } from '@/src/components/form/fields/TextField';
+import { EmailField } from '@/src/components/form/fields/EmailField';
 import { SupportedTypes } from '@/src/components/form/fields/types';
 import React from 'react';
 
@@ -17,7 +16,7 @@ export const fieldsMap: Record<SupportedTypes, React.ComponentType<any>> = {
   checkbox: CheckBoxField,
   text: TextField,
   email: EmailField,
-  money: MoneyField,
+  money: NumberField,
   select: SelectField,
   radio: RadioGroupField,
   number: NumberField,
