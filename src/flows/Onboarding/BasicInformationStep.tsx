23d24a0bce57683ca0effdf47ccdc74a09250c0f
Gabriel García Seco <ggarciaseco@gmail.com>
2025-05-13 11:45:37 +0200

    feat(onboarding) - create structure (#139)
    * feat(onboarding) - create structure
    
    * change naming
    
    * remove extra docs
    
    * fix errors
    
    * fix demo
    
    * create steps
    
    * revert
    
    * revert app
    
    ---------
    
    Co-authored-by: Gabriel Garcia <gabriel.garcia@remote.com>

---
+++ src/flows/Onboarding/BasicInformationStep.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useOnboardingContext } from './context';
+import { OnboardingForm } from '@/src/flows/Onboarding/OnboardingForm';
+
+type BasicInformationStepProps = {
+  /*
+   * The function is called when the form is submitted. It receives the form values as an argument.
+   */
+  onSubmit?: (payload: unknown) => void | Promise<void>;
+};
+
+export function BasicInformationStep({ onSubmit }: BasicInformationStepProps) {
+  const { onboardingBag } = useOnboardingContext();
+  const handleSubmit = async (values: unknown) => {
+    await onSubmit?.(onboardingBag?.parseFormValues(values) as unknown);
+    onboardingBag?.next();
+  };
+
+  return <OnboardingForm onSubmit={handleSubmit} />;
+}
---
+++ src/flows/Onboarding/OnboardingForm.tsx
@@ -0,0 +1,57 @@
+import { JSONSchemaFormFields } from '@/src/components/form/JSONSchemaForm';
+import { Form } from '@/src/components/ui/form';
+import React, { useEffect } from 'react';
+import { useOnboardingContext } from './context';
+import { useForm } from 'react-hook-form';
+import { useJsonSchemasValidationFormResolver } from '@/src/components/form/yupValidationResolver';
+import { Fields } from '@remoteoss/json-schema-form';
+
+type OnboardingFormProps = {
+  onSubmit: (payload: unknown) => void;
+  fields?: Fields;
+};
+
+export function OnboardingForm({ fields, onSubmit }: OnboardingFormProps) {
+  const { formId, onboardingBag } = useOnboardingContext();
+
+  const resolver = useJsonSchemasValidationFormResolver(
+    // @ts-expect-error no matching type
+    onboardingBag.handleValidation,
+  );
+
+  const form = useForm({
+    resolver,
+    defaultValues: onboardingBag?.initialValues,
+    shouldUnregister: false,
+    mode: 'onBlur',
+  });
+
+  useEffect(() => {
+    const subscription = form?.watch((values) => {
+      const isAnyFieldDirty = Object.keys(values).some(
+        (key) =>
+          values[key as keyof unknown] !==
+          onboardingBag?.initialValues?.[key as keyof unknown],
+      );
+      if (isAnyFieldDirty) {
+        onboardingBag?.checkFieldUpdates(values);
+      }
+    });
+    return () => subscription?.unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const jsonSchemaFields = fields ? fields : (onboardingBag?.fields ?? []);
+
+  return (
+    <Form {...form}>
+      <form
+        id={formId}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 RemoteFlows__OnboardingForm"
+      >
+        <JSONSchemaFormFields fields={jsonSchemaFields} />
+      </form>
+    </Form>
+  );
+}
