d83b6086a2c0809b5ec3d2fde52daa9249a38b13
Gabriel Garcia <gabriel.garcia@remote.com>
2025-05-13 20:37:35 +0200

    correct types

--- src/flows/Onboarding/BasicInformationStep.tsx
+++ src/flows/Onboarding/BasicInformationStep.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { useOnboardingContext } from './context';
 import { OnboardingForm } from '@/src/flows/Onboarding/OnboardingForm';
 
+type BasicInformationFormPayload = {
+  name: string;
+  email: string;
+  work_email: string;
+  job_title: string;
+  tax_servicing_countries: string[];
+  tax_job_category: string;
+  department: {
+    id: string;
+    name?: string;
+  };
+  provisional_start_date: string;
+  has_seniority_date: 'yes' | 'no';
+  manager: {
+    id: string;
+  };
+  seniority_date: string;
+};
+
 type BasicInformationStepProps = {
   /*
    * The function is called when the form is submitted. It receives the form values as an argument.
    */
-  onSubmit?: (payload: unknown) => void | Promise<void>;
+  onSubmit?: (payload: BasicInformationFormPayload) => void | Promise<void>;
   /*
    * The function is called when an error occurs during form submission.
    */
@@ -18,8 +37,9 @@ export function BasicInformationStep({
   onError,
 }: BasicInformationStepProps) {
   const { onboardingBag } = useOnboardingContext();
-  const handleSubmit = async (values: Record<string, unknown>) => {
+  const handleSubmit = async (payload: Record<string, unknown>) => {
     try {
+      const values = payload as BasicInformationFormPayload;
       await onSubmit?.(values);
       const response = await onboardingBag.onSubmit(values);
       if (response.data) {
