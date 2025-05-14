81978a5402c65a66aa5d86bce23b2719b2f0bb32
Gabriel Garcia <gabriel.garcia@remote.com>
2025-05-14 10:24:35 +0200

    fix versioning

--- src/flows/Onboarding/hooks.ts
+++ src/flows/Onboarding/hooks.ts
@@ -46,6 +46,8 @@ const useJSONSchemaForm = ({
         path: {
           country_code: countryCode,
           form: form,
+        },
+        query: {
           ...jsonSchemaQueryParam,
         },
       });
