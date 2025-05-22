7ab135d1a34a3fd968707af4e28c526f2a4f0d8e
Joao Almeida <joao.mnp.almeida@gmail.com>
2025-05-22 14:42:52 +0100

    handle undefined statement gracefully

--- src/components/form/fields/ForcedValueField.tsx
+++ src/components/form/fields/ForcedValueField.tsx
@@ -26,16 +26,25 @@ export function ForcedValueField({
 
   return (
     <div>
-      <p
-        className={`text-sm RemoteFlows__ForcedValue__Title__${name}`}
-        dangerouslySetInnerHTML={{ __html: statement.title }}
-      />
-      <p
-        className={`text-xs RemoteFlows__ForcedValue__Description__${name}`}
-        dangerouslySetInnerHTML={{
-          __html: statement.description || description,
-        }}
-      />
+      {statement ? (
+        <>
+          <p
+            className={`text-sm RemoteFlows__ForcedValue__Title__${name}`}
+            dangerouslySetInnerHTML={{ __html: statement?.title }}
+          />
+          <p
+            className={`text-xs RemoteFlows__ForcedValue__Description__${name}`}
+            dangerouslySetInnerHTML={{
+              __html: statement?.description,
+            }}
+          />
+        </>
+      ) : (
+        <p
+          className={`text-xs RemoteFlows__ForcedValue__Description__${name}`}
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      )}
     </div>
   );
 }
