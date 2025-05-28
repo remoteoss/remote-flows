6d76a95c61aa3bcc52bb5003198d8b9566179400
Joao Almeida <joao.mnp.almeida@gmail.com>
2025-05-28 08:21:19 +0100

    fix type

--- src/flows/Termination/utils.ts
+++ src/flows/Termination/utils.ts
@@ -6,7 +6,7 @@ export type StepTerminationKeys =
   | 'paid_time_off'
   | 'additional_information';
 
-export const STEPS: Record<StepTerminationKeys, Step> = {
+export const STEPS: Record<StepTerminationKeys, Step<StepTerminationKeys>> = {
   employee_communication: { index: 0, name: 'employee_communication' },
   termination_details: { index: 1, name: 'termination_details' },
   paid_time_off: { index: 2, name: 'paid_time_off' },
