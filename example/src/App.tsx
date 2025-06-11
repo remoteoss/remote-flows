c17aad05aa4bcf50435d7d186881435dc58a5ab0
Gabriel Garcia <gabriel.garcia@remote.com>
2025-06-11 17:35:35 +0200

    add source code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -893,6 +893,81 @@ export function ContractAmendment() {
   );
 }
 `,
+  costCalculatorWithReplaceableComponents: `
+  import {
+  CostCalculatorFlow,
+  CostCalculatorForm,
+  CostCalculatorSubmitButton,
+  CostCalculatorResetButton,
+  RemoteFlows,
+  CostCalculatorDisclaimer,
+} from '@remoteoss/remote-flows';
+import { components } from './Components';
+import './App.css';
+
+const fetchToken = () => {
+  return fetch('/api/token')
+    .then((res) => res.json())
+    .then((data) => ({
+      accessToken: data.access_token,
+      expiresIn: data.expires_in,
+    }))
+    .catch((error) => {
+      console.error({ error });
+      throw error;
+    });
+};
+
+const estimationOptions = {
+  title: 'Estimate for a new company',
+  includeBenefits: true,
+  includeCostBreakdowns: true,
+};
+
+export const CostCalculatorWithReplaceableComponents = () => {
+  const onReset = () => {
+    console.log('Reset button clicked');
+    // Add your reset logic here
+  };
+
+  return (
+    <RemoteFlows components={components} auth={fetchToken}>
+      <CostCalculatorFlow
+        estimationOptions={estimationOptions}
+        render={(props) => {
+          if (props.isLoading) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <div>
+              <CostCalculatorForm
+                onSubmit={(payload) => console.log(payload)}
+                onError={(error) => console.error({ error })}
+                onSuccess={(response) => console.log({ response })}
+              />
+              <div className="buttons-container">
+                <CostCalculatorResetButton
+                  className="reset-button"
+                  onClick={onReset}
+                >
+                  Reset
+                </CostCalculatorResetButton>
+                <CostCalculatorSubmitButton
+                  className="submit-button"
+                  disabled={props.isSubmitting}
+                >
+                  Get estimate
+                </CostCalculatorSubmitButton>
+              </div>
+            </div>
+          );
+        }}
+      />
+      <CostCalculatorDisclaimer label="Disclaimer" />
+    </RemoteFlows>
+  );
+};
+  `,
 };
 
 const demoStructure = [
@@ -949,7 +1024,7 @@ const demoStructure = [
         title: 'Replacable components',
         description: 'Cost Calculator with replacable components',
         component: CostCalculatorWithReplaceableComponents,
-        sourceCode: ``,
+        sourceCode: sourceCode.costCalculatorWithReplaceableComponents,
       },
     ],
   },
