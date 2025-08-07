1aa5d5d5751d020de226ff8691c7209a71ecea7c
Gabriel García Seco <ggarciaseco@gmail.com>
2025-08-07 19:47:09 +0200

    add external_id as an option (#378)
    Co-authored-by: Gabriel Garcia <gabriel.garcia@remote.com>

--- src/flows/Onboarding/types.ts
+++ src/flows/Onboarding/types.ts
@@ -17,6 +17,12 @@ export type OnboardingFlowParams = {
    * The company id to use for the onboarding.
    */
   companyId: string;
+
+  /**
+   * Unique reference code for the employment record in a non-Remote system. This optional field links to external data sources.
+   * If not provided, it defaults to null. While uniqueness is recommended, it is not strictly enforced within Remote's system.
+   */
+  externalId?: string;
   /**
    * The steps to skip for the onboarding. We only support skipping the select_country step for now.
    */
