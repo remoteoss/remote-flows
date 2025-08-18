7946946c43cf26410ba4b759a58acbd26fb57e04
Gabriel Garcia <gabriel.garcia@remote.com>
2025-08-18 17:02:19 +0200

    reorder

--- src/components/shared/zendesk-drawer/utils.ts
+++ src/components/shared/zendesk-drawer/utils.ts
@@ -1,6 +1,6 @@
 export const zendeskArticles = {
-  extraPayments: 4466822781709,
   disclaimerCostCalculator: 4668194326797,
+  extraPayments: 4466822781709,
   internationalPricing: 4410698586893,
   remoteFxRate: 33271144977421,
 };
