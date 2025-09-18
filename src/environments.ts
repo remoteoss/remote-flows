3ceb1b8ebdd09802e1e9492ffd28c19a3cf22206
Gabriel García Seco <ggarciaseco@gmail.com>
2025-09-18 14:56:31 +0200

    setup local env (#433)
    Co-authored-by: Gabriel Garcia <gabriel.garcia@remote.com>

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,5 @@
 export const ENVIRONMENTS = {
+  local: 'http://localhost:4000/api/eor',
   partners: 'https://gateway.partners.remote-sandbox.com',
   production: 'https://gateway.remote.com',
   sandbox: 'https://gateway.remote-sandbox.com',
