cd54d9e0a0abc1f34716eb1cd0afb6235f8124c8
Gabriel Garcia <gabriel.garcia@remote.com>
2025-09-30 18:10:49 +0200

    edit correctly the management fee

--- src/flows/CostCalculator/CostCalculatorFlow.tsx
+++ src/flows/CostCalculator/CostCalculatorFlow.tsx
@@ -43,6 +43,12 @@ export type CostCalculatorFlowProps = {
      * Default value for the hiring budget field
      */
     hiringBudget: string;
+    /**
+     * Default value for the management fee field.
+     */
+    management: {
+      management_fee: string;
+    };
   }>;
   options?: UseCostCalculatorOptions;
   render: (
@@ -68,6 +74,22 @@ const getDefaultManagementFee = (
   return 0;
 };
 
+const getManagementFee = (
+  currency: CurrencyKey,
+  currencySlug?: string,
+  management?: { management_fee: string },
+  managementFees?: Partial<Record<CurrencyKey, number>>,
+) => {
+  if (!currencySlug && !management?.management_fee && currency) {
+    return getDefaultManagementFee(BASE_RATES, currency, managementFees);
+  }
+  if (currencySlug && !management?.management_fee) {
+    return '';
+  }
+
+  return management?.management_fee;
+};
+
 export const CostCalculatorFlow = ({
   estimationOptions = defaultEstimationOptions,
   defaultValues = {
@@ -75,6 +97,9 @@ export const CostCalculatorFlow = ({
     currencySlug: '',
     salary: '',
     benefits: {},
+    management: {
+      management_fee: '',
+    },
   },
   options,
   render,
@@ -109,13 +134,14 @@ export const CostCalculatorFlow = ({
     costCalculatorBag.handleValidation,
   );
 
-  const defaultManagementFee = defaultValues.currencySlug
-    ? ''
-    : getDefaultManagementFee(
-        BASE_RATES,
-        currency,
-        estimationOptions.managementFees,
-      );
+  const defaultManagementFee = getManagementFee(
+    currency,
+    defaultValues.currencySlug,
+    defaultValues.management,
+    estimationOptions.managementFees,
+  );
+
+  console.log('defaultManagementFee', defaultManagementFee);
 
   const form = useForm({
     resolver,
@@ -141,7 +167,8 @@ export const CostCalculatorFlow = ({
     if (
       defaultValues.currencySlug &&
       costCalculatorBag.currencies &&
-      estimationOptions.includeManagementFee
+      estimationOptions.includeManagementFee &&
+      !defaultValues.management?.management_fee
     ) {
       const currencyData = costCalculatorBag.currencies.find(
         (currency) => currency.value === defaultValues.currencySlug,
@@ -162,6 +189,7 @@ export const CostCalculatorFlow = ({
     costCalculatorBag.currencies,
     estimationOptions.includeManagementFee,
     estimationOptions.managementFees,
+    defaultValues.management?.management_fee,
     form,
   ]);
 
