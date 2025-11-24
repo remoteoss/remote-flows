cae85a8da56c91e7e92750493b89ca0be34c8e21
Gabriel Garcia <ggarciaseco@gmail.com>
2025-11-24 18:15:31 +0100

    refactor(contractor-onboarding-api): reorganize imports and add return type annotation

--- src/flows/ContractorOnboarding/api.ts
+++ src/flows/ContractorOnboarding/api.ts
@@ -1,3 +1,6 @@
+import { createHeadlessForm, modify } from '@remoteoss/json-schema-form-next';
+import { useMutation, useQuery, UseQueryResult } from '@tanstack/react-query';
+import { FieldValues } from 'react-hook-form';
 import {
   CreateContractDocument,
   getShowContractDocument,
@@ -13,14 +16,14 @@ import { convertToCents } from '@/src/components/form/utils';
 import { useClient } from '@/src/context';
 import { signatureSchema } from '@/src/flows/ContractorOnboarding/json-schemas/signature';
 import { selectContractorSubscriptionStepSchema } from '@/src/flows/ContractorOnboarding/json-schemas/selectContractorSubscriptionStep';
-import { FlowOptions } from '@/src/flows/types';
+import {
+  FlowOptions,
+  JSONSchemaFormResultWithFieldsets,
+} from '@/src/flows/types';
 import { findFieldsByType } from '@/src/flows/utils';
 import { formatCurrency } from '@/src/lib/utils';
 import { JSFFieldset } from '@/src/types/remoteFlows';
 import { Client } from '@hey-api/client-fetch';
-import { createHeadlessForm, modify } from '@remoteoss/json-schema-form';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { FieldValues } from 'react-hook-form';
 import { corProductIdentifier } from '@/src/flows/ContractorOnboarding/constants';
 
 /**
@@ -204,7 +207,7 @@ export const useContractorOnboardingDetailsSchema = ({
   fieldValues: FieldValues;
   options?: FlowOptions & { queryOptions?: { enabled?: boolean } };
   query?: Record<string, unknown>;
-}) => {
+}): UseQueryResult<JSONSchemaFormResultWithFieldsets> => {
   const jsonSchemaQueryParam = options?.jsonSchemaVersion
     ?.contractor_contract_details_form_schema
     ? {
