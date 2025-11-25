cbc446b7642190f52bb8d755ef89deaafb276b0a
Gabriel Garcia <ggarciaseco@gmail.com>
2025-11-25 22:30:40 +0100

    chore(pdf-preview): remove debug console.log statement

--- src/components/shared/pdf-preview/LazyLoadPdfPreview.tsx
+++ src/components/shared/pdf-preview/LazyLoadPdfPreview.tsx
@@ -7,7 +7,6 @@ const PdfPreview = lazy(() =>
 );
 
 export const LazyPdfPreview: React.FC<PDFPreviewProps> = (props) => {
-  console.log('LazyPdfPreview', props);
   return (
     <Suspense fallback={<div className='animate-pulse'>Loading PDF...</div>}>
       <PdfPreview {...props} />
