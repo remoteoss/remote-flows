21cb9551473ca1c0a3c36ca8cafd4c4a750352ac
Gabriel Garcia <ggarciaseco@gmail.com>
2025-12-15 15:22:22 +0100

    feat(contractor-onboarding) - set provisional_dates inside the flow

--- src/flows/ContractorOnboarding/tests/ContractorOnboarding.test.tsx
+++ src/flows/ContractorOnboarding/tests/ContractorOnboarding.test.tsx
@@ -435,6 +435,54 @@ describe('ContractorOnboardingFlow', () => {
     await fillCountry('Portugal');
   });
 
+  it('should set provisional_start_date to today when using the form for the first time', async () => {
+    render(<ContractorOnboardingFlow {...defaultProps} />, {
+      wrapper: TestProviders,
+    });
+
+    await waitForElementToBeRemoved(() => screen.getByTestId('spinner'));
+
+    await fillCountry('Portugal');
+
+    await screen.findByText(/Step: Basic Information/i);
+
+    await waitFor(() => {
+      expect(screen.getByLabelText(/Full name/i)).toBeInTheDocument();
+    });
+
+    const provisionalStartDateInput = await screen.findByTestId(
+      'provisional_start_date',
+    );
+    const today = new Date().toISOString().split('T')[0];
+    expect(provisionalStartDateInput).toHaveValue(today);
+  });
+
+  it('should set provisional_start_date in the statement of work when using the form for the first time', async () => {
+    render(<ContractorOnboardingFlow {...defaultProps} />, {
+      wrapper: TestProviders,
+    });
+
+    await waitForElementToBeRemoved(() => screen.getByTestId('spinner'));
+
+    await fillCountry('Portugal');
+
+    // setting same value that the mock receives
+    await fillBasicInformation({
+      provisionalStartDate: '2025-11-26',
+    });
+
+    const nextButton = screen.getByText(/Next Step/i);
+    nextButton.click();
+
+    await screen.findByText(/Step: Contract Details/i);
+
+    const serviceDurationInput = await screen.findByTestId(
+      'service_duration.provisional_start_date',
+    );
+    // this value comes from the HTTP mocked call
+    expect(serviceDurationInput).toHaveValue('2025-11-26');
+  });
+
   it('should call POST /employments when country is changed and basic information is resubmitted', async () => {
     const postSpy = vi.fn();
 
