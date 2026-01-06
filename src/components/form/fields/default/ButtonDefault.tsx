cc168e73b2341426bed3250572cfc8e967f1a79e
Gabriel Garcia <ggarciaseco@gmail.com>
2026-01-06 11:18:15 +0100

    fix(button-default): add size prop to inline button variant

--- src/components/form/fields/default/ButtonDefault.tsx
+++ src/components/form/fields/default/ButtonDefault.tsx
@@ -5,7 +5,7 @@ export const ButtonDefault = (props: ButtonComponentProps) => {
   const { 'data-type': dataType, ...buttonProps } = props;
 
   if (dataType === 'inline') {
-    return <Button variant='link' {...buttonProps} />;
+    return <Button size='link' variant='link' {...buttonProps} />;
   }
 
   return <Button {...buttonProps} />;
