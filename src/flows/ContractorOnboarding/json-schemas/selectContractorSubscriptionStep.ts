923ef2ad7e13f56c23152bd0772167b92114e0fc
Gabriel Garcia <ggarciaseco@gmail.com>
2026-01-13 13:13:06 +0100

    revert file

--- src/flows/ContractorOnboarding/json-schemas/selectContractorSubscriptionStep.ts
+++ src/flows/ContractorOnboarding/json-schemas/selectContractorSubscriptionStep.ts
@@ -1,8 +1,3 @@
-import {
-  contractorPlusProductIdentifier,
-  contractorStandardProductIdentifier,
-} from '@/src/flows/ContractorOnboarding/constants';
-
 export const selectContractorSubscriptionStepSchema = {
   data: {
     version: 7,
@@ -11,40 +6,8 @@ export const selectContractorSubscriptionStepSchema = {
       properties: {
         subscription: {
           title: 'Payment terms',
-          description: 'Choose the plan that best fits your needs.',
           type: 'string',
-          oneOf: [
-            {
-              const: contractorStandardProductIdentifier,
-              title: 'Contractor Management',
-              description: 'Engage and pay contractors compliantly',
-              'x-jsf-presentation': {
-                meta: {
-                  features: [
-                    'Contract between you and contractor',
-                    'Contract service agreement template',
-                    'Only pay for active contractors',
-                    'Work with contractors in 200+ countries and jurisdictions',
-                  ],
-                },
-              },
-            },
-            {
-              const: contractorPlusProductIdentifier,
-              title: 'Contractor Management Plus',
-              description: 'Engage and pay contractors with indemnity coverage',
-              'x-jsf-presentation': {
-                meta: {
-                  features: [
-                    'Contract between you and contractor',
-                    'Access to all Contractor Management features',
-                    '$100K indemnity coverage for penalties',
-                    '4X monetary coverage than our competitors  in case of penalties',
-                  ],
-                },
-              },
-            },
-          ],
+          oneOf: [],
           'x-jsf-presentation': {
             inputType: 'radio',
           },
