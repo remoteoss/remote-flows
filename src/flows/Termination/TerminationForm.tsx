76a82531bd0e3dee9f1eb633b46dc53c3815959d
Gabriel Garcia <ggarciaseco@gmail.com>
2026-04-20 11:27:36 +0200

    fix

--- src/flows/Termination/TerminationForm.tsx
+++ src/flows/Termination/TerminationForm.tsx
@@ -5,6 +5,7 @@ import { useTerminationContext } from './context';
 import { TerminationFormValues } from '@/src/flows/Termination/types';
 import { useJSONSchemaForm } from '@/src/components/form/useJSONSchemaForm';
 import { FieldValues } from 'react-hook-form';
+import { ValidationResult } from '@remoteoss/remote-json-schema-form-kit';
 
 type TerminationFormProps = {
   onSubmit: (payload: TerminationFormValues) => void;
@@ -19,10 +20,16 @@ export function TerminationForm({
 }: TerminationFormProps) {
   const { formId, terminationBag } = useTerminationContext();
 
+  // casting handleValidation and checkFieldUpdates to the correct type
+  // my reasoning is partners could use these functions directly and I want to avoid breaking changes on types
   const form = useJSONSchemaForm({
-    handleValidation: terminationBag.handleValidation,
+    handleValidation: terminationBag.handleValidation as (
+      values: FieldValues,
+    ) => Promise<ValidationResult | null>,
     defaultValues: defaultValues || {},
-    checkFieldUpdates: terminationBag.checkFieldUpdates,
+    checkFieldUpdates: terminationBag.checkFieldUpdates as (
+      values: FieldValues,
+    ) => void,
   });
 
   const jsonSchemaFields = fields ? fields : (terminationBag?.fields ?? []);
