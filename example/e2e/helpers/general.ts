d72ee997d48fea10dea33bb7b42cdf1f4f805a82
Jordi Vidaller <jordi.vidaller@remote.com>
2026-05-12 18:44:15 +0200

    reorganize e2e fill form helpers + add first step

--- example/e2e/helpers/general.ts
+++ example/e2e/helpers/general.ts
@@ -1,32 +1,5 @@
 import { Page, Route } from '@playwright/test';
 
-interface FillEstimationFormOptions {
-  country: string;
-  currency: string;
-  salary: string;
-  management_fee: string;
-}
-
-export async function fillEstimationForm(
-  page: Page,
-  options: Partial<FillEstimationFormOptions>,
-) {
-  if (options.country) {
-    await page.selectOption('#country', options.country);
-  }
-  if (options.currency) {
-    await page.selectOption('#currency', options.currency);
-  }
-  if (options.salary) {
-    await page.fill('#salary_conversion', options.salary);
-  }
-  if (options.management_fee) {
-    await page.getByRole('button', { name: 'Show Management fee' }).click();
-    await page.fill('#management\\.management_fee', options.management_fee);
-  }
-  await page.click('.submit-button');
-}
-
 export async function setupVercelBypass(page: Page) {
   await page.route('**/*', async (route: Route) => {
     const url = route.request().url();
