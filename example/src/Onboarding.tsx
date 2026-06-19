85f658108fcfd31a58827517015af756badb0aad
Gabriel Garcia <ggarciaseco@gmail.com>
2026-06-19 13:18:47 +0200

    revert

--- example/src/Onboarding.tsx
+++ example/src/Onboarding.tsx
@@ -29,7 +29,6 @@ import { transformHtmlToComponents } from './utils/transformHtml';
 import { sanitizeHtml } from '@remoteoss/remote-flows/internals';
 import { downloadFile } from './utils';
 import './css/main.css';
-import { components } from './Components';
 
 const BenefitsAboutSection = ({
   description,
@@ -553,7 +552,6 @@ const OnboardingWithProps = ({
   <RemoteFlows
     proxy={{ url: window.location.origin }}
     transformHtmlToComponents={transformHtmlToComponents}
-    components={components}
   >
     <OnboardingFlow
       companyId={companyId}
