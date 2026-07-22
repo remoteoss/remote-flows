e8f0321a9a9904054ce06f70e707197d0bd4c2f1
Gabriel Garcia <ggarciaseco@gmail.com>
2026-07-22 09:31:55 +0200

    revert

--- src/common/createHeadlessForm.tsx
+++ src/common/createHeadlessForm.tsx
@@ -3,10 +3,12 @@ import {
   createHeadlessForm as baseCreateHeadlessForm,
   modify,
 } from '@remoteoss/remote-json-schema-form-kit';
+import { convertToCents } from '@/src/components/form/utils';
 import {
   JSFModify,
   JSONSchemaFormResultWithFieldsets,
 } from '@/src/flows/types';
+import { findFieldsByType } from '@/src/flows/utils';
 import { JSFFieldset } from '@/src/types/remoteFlows';
 
 /*
@@ -57,6 +59,19 @@ export const createHeadlessForm = (
     }
   }
 
+  let moneyFieldsData: Record<string, number | null> = {};
+
+  if (fieldValues) {
+    const moneyFields = findFieldsByType(jsfSchema.properties || {}, 'money');
+    moneyFieldsData = moneyFields.reduce<Record<string, number | null>>(
+      (acc, field) => {
+        acc[field] = convertToCents(fieldValues[field]);
+        return acc;
+      },
+      {},
+    );
+  }
+
   /**
    * We create a deep copy of the field values to avoid modifying the original object.
    * This problem is caused by json-schema-form-v0.
@@ -64,6 +79,7 @@ export const createHeadlessForm = (
   const initialValues = JSON.parse(
     JSON.stringify({
       ...fieldValues,
+      ...moneyFieldsData,
     }),
   );
 
