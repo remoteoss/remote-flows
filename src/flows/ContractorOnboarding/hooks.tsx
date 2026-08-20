198d6d9d3033cc950b1a1164d4abc463d89e48d8
Gabriel García Seco <ggarciaseco@gmail.com>
2026-08-20 16:05:24 +0200

    feat(invoice-schedule) - prefill selection (#1257)
    * feat(invoice-schedule) - prefill selection
    
    * fix call

--- src/flows/ContractorOnboarding/hooks.tsx
+++ src/flows/ContractorOnboarding/hooks.tsx
@@ -716,7 +716,10 @@ export const useContractorOnboarding = ({
     employmentId: internalEmploymentId as string,
     options: {
       queryOptions: {
-        enabled: includeInvoiceSchedule && Boolean(internalEmploymentId),
+        enabled:
+          includeInvoiceSchedule &&
+          Boolean(internalEmploymentId) &&
+          stepState.currentStep.name === 'invoice_schedule',
       },
     },
   });
@@ -877,9 +880,17 @@ export const useContractorOnboarding = ({
   const invoiceScheduleInitialValues = useMemo(() => {
     const initialValues = {
       ...onboardingInitialValues,
+      // If an existing schedule exists, default to 'schedule' preference
+      ...(existingInvoiceSchedule && {
+        invoice_schedule_preference: 'schedule',
+      }),
     };
     return getInitialValues(stepFields.invoice_schedule, initialValues);
-  }, [stepFields.invoice_schedule, onboardingInitialValues]);
+  }, [
+    stepFields.invoice_schedule,
+    onboardingInitialValues,
+    existingInvoiceSchedule,
+  ]);
 
   const createInvoiceScheduleInitialValues = useMemo(() => {
     // If an existing schedule exists, transform it to form values
